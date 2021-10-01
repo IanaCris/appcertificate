78a02c2328fc44a1a087d6b10edfa8c9d8b590ae
IanaCris <ianasousati@gmail.com>
2021-09-30 23:38:58 -0300

    convert pdf

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -65,7 +65,29 @@ export const handle = async (event) => {
   //////Gera o certificado//////
   //Compilar usando handlebars
   const content = await compile(data);
+  
   //Transformar em pdf
+  const browser = await chromium.puppeteer.launch({
+    headless: true,
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath
+  });
+
+  const page = await browser.newPage();
+
+  await page.setContent(content);
+
+  const pdf = await page.pdf({
+    format: "a4",
+    landscape: true,
+    path: process.env.IS_OFFLINE ? "certificate.pdf" : null,
+    printBackground: true,
+    preferCSSPageSize: true
+  });
+
+  await browser.close();
+
   //salvar no s3
 
 
